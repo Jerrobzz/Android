30ebebb86f8f6aa5155dc0869432d1c0f15d95f8
eMobilis <jeremykipkirui254@gmail.com>
2023-05-10 16:27:44 +0300

    first commit

---
+++ app/src/main/java/com/example/jeremy_001/PageLinearActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jeremy_001
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PageLinearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_linear)
+    }
+}
